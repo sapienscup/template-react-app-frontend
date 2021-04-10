a9449dd3772af9bc7dc9e249b367943510aca210
Lucas <lucas@prevision.com.br>
2021-04-10 20:14:48 -0300

    Deleted: --put-to-work :man_health_worker:

--- components/atoms/ListItemText/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ListItemText from '.';
-
-storiesOf('atoms/ListItemText', module)
-  .add('default', () => (
-    <ListItemText primary="Default" />
-  ));
