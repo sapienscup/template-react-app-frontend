a5b42801d631247a85fdc355701dbdad2a7243f1
Lucas <lucas@prevision.com.br>
2021-04-10 20:14:50 -0300

    New file --put-to-work :man_health_worker:

---
+++ src/stories/examples/Button.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+
+import { ExampleButton, ExampleButtonProps } from './Button';
+
+export default {
+  title: 'Examples/Atoms/Button',
+  component: ExampleButton,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template: Story<ExampleButtonProps> = (args) => <ExampleButton {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'ExampleButton',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Example Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Example Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Example Button',
+};
