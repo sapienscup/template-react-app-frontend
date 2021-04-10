509a5b4fd2f3b708ad1da48e33a8ad7e2f44b4f8
Lucas <lucas@prevision.com.br>
2021-04-10 20:14:49 -0300

    Deleted: --put-to-work :man_health_worker:

--- components/moleculus/SimpleCard/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { SimpleCard } from '../..';
-
-storiesOf('moleculus/SimpleCard', module)
-  .add('default', () => (
-    <SimpleCard title="Default title" description="Default description" />
-  ));
