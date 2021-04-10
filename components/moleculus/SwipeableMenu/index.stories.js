19cdd1e2d28f55aac17bfe9d84c69dc2879782d2
Lucas <lucas@prevision.com.br>
2021-04-10 20:14:49 -0300

    Deleted: --put-to-work :man_health_worker:

--- components/moleculus/SwipeableMenu/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { SwipeableMenu } from '../..';
-
-storiesOf('moleculus/SwipeableMenu', module)
-  .add('opened by default', () => (
-    <SwipeableMenu isOpenedByDefault menuItems={['First', 'Second', 'Third']} />
-  ));
