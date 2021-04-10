00c298aa691c6234d3b9d3a4012dcfe686dd6a1e
Lucas <lucas@prevision.com.br>
2021-04-10 20:14:49 -0300

    New file --put-to-work :man_health_worker:

---
+++ src/components/atoms/NiceButton/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
+
+export interface NiceButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const NiceButton: React.FC<NiceButtonProps> = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+NiceButton.propTypes = {
+  primary: PropTypes.bool,
+  backgroundColor: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func
+};
+
+NiceButton.defaultProps = {
+  primary: false,
+  backgroundColor: '',
+  size: 'medium',
+  label: '',
+  onClick: () => {return;}
+};
